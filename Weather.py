1f6946d1619dacc07a87aac261f240f6429b6af5
QQnewuser <142733952+QQnewuser@users.noreply.github.com>
2023-10-18 20:49:06 +0800

    Update Weather.py

--- Weather.py
+++ Weather.py
@@ -2,81 +2,81 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 import re
-# 读取xlsx文件到DataFrame对象
-df = pd.read_excel('city/'+'武汉'+ "近5年天气数据.xlsx")
+from matplotlib.font_manager import FontProperties
 
-# 统计某一列数据的权重
-weights = {}
-translation = {
-               '暴雨':'big-rain',
-               '小雨': 'light-rain',
-               '雾': 'fog',
-               '晴':'sunny',
-               '霾':'haze',
-               '多云':'cloudy',
-               '阴':'cloudy',
-               '阵雨':'shower',
-               '雷阵雨':'thunder-storm',
-               '雨夹雪':'sleet',
-               '小雪':'light-snow',
-               '大雨':'heavy-rain',
-               '浮尘':'dust',
-               '中雨':'middle-rain',
-               '小到中雨':'LTM-rain',
-               '阵雪':'snower',
-               '大到暴雨':'BTT-rain',
-               '小到中雪':'LTM-snow',
-               '中度霾':'middle-haze'
-              }
-for item in df['Weather']:
-    if '~' in item:
-        start, end = item.split('~')
-        weight = 0.5
-        try:
-            for value in range(int(start), int(end) + 1):
-                char = chr(value)  # 将Unicode编码转换为对应的汉字字符
-                translated_char = translation.get(char, char)  # 获取中文对应的英文翻译，若无则保持原字符
-                weights[translated_char] = weights.get(translated_char, 0) + weight
-        except ValueError:
-            pass
-    else:
-        translated_item = translation.get(item, item)  # 获取中文对应的英文翻译，若无则保持原字符
-        weights[translated_item] = weights.get(translated_item, 0) + 1
+font = FontProperties(fname=r"SimHei.ttf")
+plt.rcParams['font.sans-serif'] = [font.get_name()]  # 将默认字体设置为SimHei
+plt.rcParams['axes.unicode_minus'] = False  # 解决保存图像是负号'-'显示为方块的问题
 
-# 输出权重信息
-for key, value in weights.items():
-    print(f'{key}: {value}')
+def read_excel(file_name):
+    data = pd.read_excel(file_name, sheet_name=0, header=0)
 
-# 计算总权重
-total_weight = sum(weights.values())
+    # 处理MaxTemp和MinTemp，将其转换为数字格式
+    #data['Weather'] = data['Weather'].apply(lambda x: int(re.sub('[^-\\d]', '', str(x))))
+    # 返回处理后的数据
+    return data
+city_list = ['呼和浩特', '武汉', '北京', '杭州', '广州', '哈尔滨', '昆明', '南京', '福州', '海口', '合肥', '济南',
+             '南昌', '南宁', '沈阳', '太原', '天津', '西宁', '银川', '长春']
+city_dict = {'武汉': 'wuhan', '北京': 'beijing', '杭州': 'hangzhou',
+             '广州': 'guangzhou', '哈尔滨': 'haerbin', '呼和浩特': 'huhehaote',
+             '昆明': 'kunming', '南京': 'nanjin', '福州': 'fuzhou', '海口': 'haikou', '合肥': 'hefei', '济南': 'jinan',
+             '南昌': 'nanchang', '南宁': 'nanning', '沈阳': 'shenyang', '太原': 'taiyuan', '天津': 'tianjin',
+             '西宁': 'xining', '银川': 'yinchuan', '长春': 'changchun'}
+for city in city_list:
+    # 读取xlsx文件到DataFrame对象
+    file_name =  city + "近5年天气数据.xlsx"
+    weights = {}
+    data = read_excel(file_name)
+    #df = pd.read_excel(city + '近5年天气数据.xlsx')
+    #df = pd.read_excel(city + '近5年天气数据.xlsx', usecols=[4])
+    translation = {
+        '暴雨': 'rain',
+        '小雨': 'rain',
+        '雾': 'fog',
+        '晴': 'sunny',
+        '霾': 'haze',
+        '多云': 'cloudy',
+        '阴': 'cloudy',
+        '阵雨': 'rain',
+        '雷阵雨': 'rain',
+        '雨夹雪': 'snow',
+        '小雪': 'snow',
+        '中雪': 'snow',
+        '大雪': 'snow',
+        '大雨': 'rain',
+        '大暴雨': 'rain',
+        '浮尘': 'haze',
+        '中雨': 'rain',
+        '小到中雨': 'rain',
+        '阵雪': 'snow',
+        '中到大雨': 'rain',
+        '大到暴雨': 'rain',
+        '小到中雪': 'snow',
+        '中度霾': 'haze'
+    }
 
-# 计算每项的占比
-proportions = {key: value / total_weight for key, value in weights.items()}
+    for item in data['Weather']:
+        if '~' in item:
+            start, end = item.split('~')
+            weight = 0.5
+            try:
+                for value in range(int(start), int(end) + 1):
+                    char = chr(value)  # 将Unicode编码转换为对应的汉字字符
+                    translated_char = translation.get(char, char)  # 获取中文对应的英文翻译，若无则保持原字符
+                    weights[translated_char] = weights.get(translated_char, 0) + weight
+            except ValueError:
+                pass
+        else:
+            translated_item = translation.get(item, item)  # 获取中文对应的英文翻译，若无则保持原字符
+            weights[translated_item] = weights.get(translated_item, 0) + 1
+    print(weights)
+    categories = ['晴', '多云', '雨', '雪', '雾', '霾']
+    colors = ['blue', 'green', 'yellow', 'orange', 'red', 'purple']
 
-# 设置颜色映射对象
-cmap = plt.get_cmap('tab20')
-colors = [cmap(i) for i in range(len(proportions))]
-
-# 绘制饼图
-plt.pie(proportions.values(), labels=proportions.keys(), colors=colors,textprops={'fontsize':8})
-plt.axis('equal')
-
-
-# 添加每个部分的名称和占比标签
-labels = [f'{key}: {value * 100:.1f}%' for key, value in proportions.items()]
-plt.text(-1.6, -1.2, '\n'.join(labels), fontsize=12)
-
-# 获取当前坐标轴对象
-ax = plt.gca()
-
-# 构建文本标签所需的英文信息
-translated_labels = [f'{translation.get(key, key)}: {value * 100:.1f}%' for key, value in proportions.items()]
-
-# 添加文本标签显示占比信息和颜色对应关系
-x0, y0 = ax.transAxes.transform((1, 1))  # 将坐标从图形坐标系转换为坐标轴坐标系
-x, y = ax.transAxes.inverted().transform((x0, y0))  # 将右上角的点从坐标轴坐标系转换为图形坐标系
-label = '\n'.join([f'{translated_labels[i]} ({colors[i]})' for i in range(len(translated_labels))])  # 要显示的文本
-ax.text(x - 220, y - 240, label, fontsize=12, ha='left', va='center')  # 添加文本标签
-
-plt.savefig('Weather/'+'武汉'+'.png')
-plt.show()
+    fig, ax = plt.subplots()
+    ax.pie(weights.values(), labels=weights.keys(), colors=colors, autopct='%1.1f%%', startangle=90)
+    ax.legend(loc='upper right')
+    plt.axis('equal')  # 为了使饼图呈现圆形
+    plt.title(city + '天气分布')
+    plt.savefig(city + '天气分布.png')
+    plt.show()
